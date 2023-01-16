28e3d88755405f0205c8baf03b659ecfdf3417a6
shah_hoque1 <shah.hoque10@gmail.com>
2023-01-16 15:28:13 +0000

    Result toast element added. Password is parsed successfully inside the toast

--- script.js
+++ script.js
@@ -1,3 +1,224 @@
+
+const resultDisplay = document.getElementById('display-pass')
+
+
+
+// FORM CLIENT-SIDE DATA ENTRY VALIDATION -----------------------------------
+const form = document.getElementById('form')
+
+// VAR'S RELATED TO "PASSWORD LENGTH" ERRORS
+const numLenGiven = document.getElementById('num-len-given')
+const errorElement1 = document.getElementById('error_msg_1')
+
+// VAR'S RELATED TO "SWITCHES" ERRORS
+const allSwitches = document.querySelectorAll('.form-group .form-check-input');
+const errorElement2 = document.getElementById('error_msg_2');
+let atLeastOneChecked = false;
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  // ERROR CHECKS FOR "PASSWORD LENGTH"
+  let logError1 = []
+  if (numLenGiven.value === '' || numLenGiven.value == null) {
+    logError1.push('Please enter a number')
+    
+  }
+  if (numLenGiven.value >= 1) {
+    if (numLenGiven.value < 10 || numLenGiven.value > 64) {
+      logError1.push('Please enter a number between 10-64')
+    }
+  }
+  
+  if (logError1.length > 0) {
+    errorElement1.innerText = logError1.join(', ')
+  } else {
+    errorElement1.innerText = '';
+  }
+  
+  // ERROR CHECKS FOR "SWITCHES"
+  let logError2 = []
+  
+  allSwitches.forEach(input => {
+    if (input.checked) {
+      atLeastOneChecked = true;
+    }
+  });
+  
+  if (!atLeastOneChecked) {
+    logError2.push('Please select at least one option')
+  }
+  
+  if (logError2.length > 0) {
+    errorElement2.innerText = logError2.join(', ');
+  } else {
+    atLeastOneChecked = false;
+    errorElement2.innerText = '';
+  }
+})
+// FORM CLIENT-SIDE DATA ENTRY VALIDATION (end) -----------------------------------
+
+
+// GET FORM VALUES -----------------------------------
+var isCapitalLettersOn = document.getElementById('capital-letters')
+var isNumbersOn = document.getElementById('numbers')
+var isSpecialCharOn = document.getElementById('special-char')
+
+var isLowercaseOn = true
+// GET FORM VALUES (end) -----------------------------------
+
+// CREATE AN ARRAY TO HOLD THE COMPLETED PASSWORD
+var passwordBox = []
+
+// VARS THAT WILL BE ALLOCATED a # SPLIT FROM THE # DEFINED IN numLenGiven
+var addSpecialCharCut = 0
+var addNumericCharactersCut = 0
+var addLowerCasedCharactersCut = 0
+var addUpperCasedCharactersCut = 0
+
+
+// FUNC THAT SPLITS UP numLenGiven ACROSS THE SWITCHES TURNED ON -----------------------------------
+function setCuts() {
+  var count = 0;
+  
+  while (count < numLenGiven.value) {
+    
+    if (isSpecialCharOn.checked && count != numLenGiven.value) {
+      addSpecialCharCut++;
+      count++;
+    } 
+    if (isNumbersOn.checked && count != numLenGiven.value) {
+      addNumericCharactersCut++;
+      count++;
+    }
+    if (isLowercaseOn === true && count != numLenGiven.value) {
+      addLowerCasedCharactersCut++;
+      count++;
+    }
+    if (isCapitalLettersOn.checked && count != numLenGiven.value) {
+      addUpperCasedCharactersCut++;
+      count++;
+    }
+  }
+}
+// FUNC THAT SPLITS UP numLenGiven ACROSS THE SWITCHES TURNED ON (end) -----------------------------------
+
+
+// 4 FUNC'S THAT PUSH THE # OF ELEMENTS (FROM ITS SPLIT) TO passwordBox -----------------------------------
+function addSpecialChar() {
+  for (let i = 0; i < addSpecialCharCut; i++) {
+    const randomIndex = Math.floor(Math.random() * specialCharacters.length);
+    passwordBox.push(specialCharacters[randomIndex])
+  }
+}
+
+function addNumericCharacters() {
+  for (let i = 0; i < addNumericCharactersCut; i++) {
+    const randomIndex = Math.floor(Math.random() * numericCharacters.length);
+    passwordBox.push(numericCharacters[randomIndex])
+  }
+}
+
+function addLowerCasedCharacters() {
+  for (let i = 0; i < addLowerCasedCharactersCut; i++) {
+    const randomIndex = Math.floor(Math.random() * lowerCasedCharacters.length);
+    passwordBox.push(lowerCasedCharacters[randomIndex])
+  }
+}
+
+function addUpperCasedCharacters() {
+  for (let i = 0; i < addUpperCasedCharactersCut; i++) {
+    const randomIndex = Math.floor(Math.random() * upperCasedCharacters.length);
+    passwordBox.push(upperCasedCharacters[randomIndex])
+  }
+}
+// 4 FUNC'S THAT PUSH THE # OF ELEMENTS (FROM ITS SPLIT) TO passwordBox (end) -----------------------------------
+
+// VAR THAT CONTAINS THE LAST RESULT ARRAY
+var passwordBoxMadeRandom = []
+
+
+// FUNC THAT MAKES THE LAST RESULT IN passwordBox RANDOM -----------------------------------
+function passwordBoxMakeRandom() {
+  for (let i = 0; i < passwordBox.length; i++) {
+    const randomIndex = Math.floor(Math.random() * passwordBox.length);
+    passwordBoxMadeRandom.push(passwordBox[randomIndex])
+  }
+}
+// FUNC THAT MAKES THE LAST RESULT IN passwordBox RANDOM (end) -----------------------------------
+
+
+function displayResult() {
+  // const myNewString = passwordBoxMadeRandom.join(',');
+
+  resultDisplay.innerText = passwordBoxMadeRandom.join('');
+
+}
+
+
+
+
+
+// FUNC THAT RESETS THE FORM & ALL CHANGING VAR'S -----------------------------------
+function resetFormAndCutsAndResult() {
+  document.getElementById('form').reset()
+  
+  addSpecialCharCut = 0
+  addNumericCharactersCut = 0
+  addLowerCasedCharactersCut = 0
+  addUpperCasedCharactersCut = 0
+  
+  // The below two reassignments are removing the last result. It needs to be removed, then added after the result is shown.
+  
+  // add the popup, show the password, then add the 2 vars below
+  passwordBox = []
+  passwordBoxMadeRandom = []
+  
+  
+}   
+// FUNC THAT RESETS THE FORM & ALL CHANGING VAR'S (end) -----------------------------------
+
+
+
+// this eventListener is not needed and is here just to see results from my coding below
+form.addEventListener('submit', (e) => {
+  console.log(numLenGiven.value);
+  
+  console.log(isCapitalLettersOn.checked);
+  console.log(isNumbersOn.checked);
+  console.log(isSpecialCharOn.checked);
+  
+  setCuts() 
+  
+  addSpecialChar()
+  addNumericCharacters()
+  addLowerCasedCharacters()
+  addUpperCasedCharacters()
+  
+  passwordBoxMakeRandom()
+  
+  
+  displayResult()
+  
+  Toasty()
+  
+  resetFormAndCutsAndResult()
+  
+  
+  console.log("numLenGiven.value " + numLenGiven.value)
+  console.log("addSpecialCharCut: " + addSpecialCharCut)
+  console.log("addNumericCharactersCut: " + addNumericCharactersCut)
+  console.log("addLowerCasedCharactersCut: " + addLowerCasedCharactersCut)
+  console.log("addUpperCasedCharactersCut: " + addUpperCasedCharactersCut)
+  
+  console.log(passwordBox);
+  console.log(passwordBoxMadeRandom);
+  
+})
+
+
+
+
+
 // Array of special characters to be included in password
 var specialCharacters = [
     '@',
@@ -87,198 +308,3 @@ var specialCharacters = [
     'Y',
     'Z'
   ];
-
-
-// FORM CLIENT-SIDE DATA ENTRY VALIDATION -----------------------------------
-const form = document.getElementById('form')
-
-// VAR'S RELATED TO "PASSWORD LENGTH" ERRORS
-const numLenGiven = document.getElementById('num-len-given')
-const errorElement1 = document.getElementById('error_msg_1')
-
-// VAR'S RELATED TO "SWITCHES" ERRORS
-const allSwitches = document.querySelectorAll('.form-group .form-check-input');
-const errorElement2 = document.getElementById('error_msg_2');
-let atLeastOneChecked = false;
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  // ERROR CHECKS FOR "PASSWORD LENGTH"
-  let logError1 = []
-  if (numLenGiven.value === '' || numLenGiven.value == null) {
-    logError1.push('Please enter a number')
-
-  }
-  if (numLenGiven.value >= 1) {
-    if (numLenGiven.value < 10 || numLenGiven.value > 64) {
-      logError1.push('Please enter a number between 10-64')
-    }
-}
-
-  if (logError1.length > 0) {
-    errorElement1.innerText = logError1.join(', ')
-  } else {
-    errorElement1.innerText = '';
-  }
-
-  // ERROR CHECKS FOR "SWITCHES"
-  let logError2 = []
-
-  allSwitches.forEach(input => {
-  if (input.checked) {
-      atLeastOneChecked = true;
-    }
-  });
-
-  if (!atLeastOneChecked) {
-      logError2.push('Please select at least one option')
-  }
-
-  if (logError2.length > 0) {
-      errorElement2.innerText = logError2.join(', ');
-  } else {
-    atLeastOneChecked = false;
-    errorElement2.innerText = '';
-  }
-})
-// FORM CLIENT-SIDE DATA ENTRY VALIDATION (end) -----------------------------------
-
-
-// GET FORM VALUES -----------------------------------
-var isCapitalLettersOn = document.getElementById('capital-letters')
-var isNumbersOn = document.getElementById('numbers')
-var isSpecialCharOn = document.getElementById('special-char')
-
-var isLowercaseOn = true
-// GET FORM VALUES (end) -----------------------------------
-
-// CREATE AN ARRAY TO HOLD THE COMPLETED PASSWORD
-var passwordBox = []
-
-// VARS THAT WILL BE ALLOCATED a # SPLIT FROM THE # DEFINED IN numLenGiven
-var addSpecialCharCut = 0
-var addNumericCharactersCut = 0
-var addLowerCasedCharactersCut = 0
-var addUpperCasedCharactersCut = 0
-
-
-// FUNC THAT SPLITS UP numLenGiven ACROSS THE SWITCHES TURNED ON -----------------------------------
-function setCuts() {
-  var count = 0;
-
-  while (count < numLenGiven.value) {
-
-    if (isSpecialCharOn.checked && count != numLenGiven.value) {
-      addSpecialCharCut++;
-      count++;
-    } 
-    if (isNumbersOn.checked && count != numLenGiven.value) {
-      addNumericCharactersCut++;
-      count++;
-    }
-    if (isLowercaseOn === true && count != numLenGiven.value) {
-      addLowerCasedCharactersCut++;
-      count++;
-    }
-    if (isCapitalLettersOn.checked && count != numLenGiven.value) {
-      addUpperCasedCharactersCut++;
-      count++;
-    }
-  }
-}
-// FUNC THAT SPLITS UP numLenGiven ACROSS THE SWITCHES TURNED ON (end) -----------------------------------
-
-
-// 4 FUNC'S THAT PUSH THE # OF ELEMENTS (FROM ITS SPLIT) TO passwordBox -----------------------------------
-function addSpecialChar() {
-  for (let i = 0; i < addSpecialCharCut; i++) {
-    const randomIndex = Math.floor(Math.random() * specialCharacters.length);
-    passwordBox.push(specialCharacters[randomIndex])
-  }
-}
-
-function addNumericCharacters() {
-  for (let i = 0; i < addNumericCharactersCut; i++) {
-    const randomIndex = Math.floor(Math.random() * numericCharacters.length);
-    passwordBox.push(numericCharacters[randomIndex])
-  }
-}
-
-function addLowerCasedCharacters() {
-  for (let i = 0; i < addLowerCasedCharactersCut; i++) {
-    const randomIndex = Math.floor(Math.random() * lowerCasedCharacters.length);
-    passwordBox.push(lowerCasedCharacters[randomIndex])
-  }
-}
-
-function addUpperCasedCharacters() {
-  for (let i = 0; i < addUpperCasedCharactersCut; i++) {
-    const randomIndex = Math.floor(Math.random() * upperCasedCharacters.length);
-    passwordBox.push(upperCasedCharacters[randomIndex])
-  }
-}
-// 4 FUNC'S THAT PUSH THE # OF ELEMENTS (FROM ITS SPLIT) TO passwordBox (end) -----------------------------------
-
-// VAR THAT CONTAINS THE LAST RESULT ARRAY
-var passwordBoxMadeRandom = []
-
-
-// FUNC THAT MAKES THE LAST RESULT IN passwordBox RANDOM -----------------------------------
-function passwordBoxMakeRandom() {
-  for (let i = 0; i < passwordBox.length; i++) {
-    const randomIndex = Math.floor(Math.random() * passwordBox.length);
-    passwordBoxMadeRandom.push(passwordBox[randomIndex])
-  }
-}
-// FUNC THAT MAKES THE LAST RESULT IN passwordBox RANDOM (end) -----------------------------------
-
-// FUNC THAT RESETS THE FORM & ALL CHANGING VAR'S -----------------------------------
-function resetSwitchCuts() {
-  document.getElementById('form').reset()
-
-  addSpecialCharCut = 0
-  addNumericCharactersCut = 0
-  addLowerCasedCharactersCut = 0
-  addUpperCasedCharactersCut = 0
-
-  // The below two reassignments are removing the last result. It needs to be removed, then added after the result is shown.
-  passwordBox = []
-  passwordBoxMadeRandom = []
-
-  
-}   
-// FUNC THAT RESETS THE FORM & ALL CHANGING VAR'S (end) -----------------------------------
-
-
-
-// this eventListener is not needed and is here just to see results from my coding below
-form.addEventListener('submit', (e) => {
-  console.log(numLenGiven.value);
-
-console.log(isCapitalLettersOn.checked);
-console.log(isNumbersOn.checked);
-console.log(isSpecialCharOn.checked);
-
-setCuts() 
-
-addSpecialChar()
-addNumericCharacters()
-addLowerCasedCharacters()
-addUpperCasedCharacters()
-
-passwordBoxMakeRandom()
-
-resetSwitchCuts()
-
-
-
-console.log("numLenGiven.value " + numLenGiven.value)
-console.log("addSpecialCharCut: " + addSpecialCharCut)
-console.log("addNumericCharactersCut: " + addNumericCharactersCut)
-console.log("addLowerCasedCharactersCut: " + addLowerCasedCharactersCut)
-console.log("addUpperCasedCharactersCut: " + addUpperCasedCharactersCut)
-
-console.log(passwordBox);
-console.log(passwordBoxMadeRandom);
-
-})
